347ac5feeeb325580ed92e075563112f4c4c7707
Ahmed saed <85667800+ahmed2008saed@users.noreply.github.com>
2022-02-13 11:33:44 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -4,3 +4,7 @@ console.log("%cAhmed %cSa%c3%ced %cSayed"
 ,"color: #ffee00; font-size: 40px;"
 ,"color: #ccc; font-size: 40px;"
 ,"color: #555; font-size: 40px;");
+
+console.log("متكتبش حاجا هنا ماشي")
+
+console.log("Copyright © Ahmed Saed 2022")
