a7e02773a2cbce2db1bd66fbb689548a218b0a73
Ahmed saed <85667800+ahmed2008saed@users.noreply.github.com>
2022-02-14 08:54:18 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ console.log("%cCopyright %c© %cAhmed Saed 2022"
 ,"color: #555; font-size: 40px; font-family: Tahoma;");
 
 console.log("%cAhmed %cSa%c3%ced %cSayed"
-,"color: #f00; font-size: 40px; font-family: Tahoma;"
+,"color: #dc3545; font-size: 40px; font-family: Tahoma;"
 ,"color: #ccc; font-size: 40px; font-family: Tahoma;"
 ,"color: #ffee00; font-size: 40px; font-family: Tahoma;"
 ,"color: #ccc; font-size: 40px; font-family: Tahoma;"
